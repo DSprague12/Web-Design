8a7f8af446db43b3de1c37d6c1cc65ae230480e8
Dallin Sprague <61210689+DSprague12@users.noreply.github.com>
2025-07-15 16:02:28 -0600

    finished?

--- finalproject/script.js
+++ finalproject/script.js
@@ -1,19 +1,45 @@
 let slideIndex = 0;
-window.onload = function(){
+
+window.onload = function () {
     showSlide(slideIndex);
-}
-function modifySlide(change){
-    slideIndex+=change;
+};
+
+function modifySlide(change) {
+    let slides = document.getElementsByClassName("slides");
+    let dots = document.getElementsByClassName("dot");
+
+    slideIndex += change;
+    if (slideIndex >= slides.length) slideIndex = 0;
+    if (slideIndex < 0) slideIndex = slides.length - 1;
+
     showSlide(slideIndex);
+
+    updateDots(change);
 }
 
-function showSlide(index){
+function showSlide(index) {
     let slides = document.getElementsByClassName("slides");
-    if(slideIndex>slides.length-2) slideIndex = slides.length-2;
-    if(slideIndex<1) slideIndex=1;
-    for (let i = 0; i < slides.length; i++)
-    {
+
+    for (let i = 0; i < slides.length; i++) {
         slides[i].classList.remove("active");
     }
+
     slides[index].classList.add("active");
+}
+
+function updateDots(direction) {
+    let dots = document.getElementsByClassName("dot");
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+
+    if (direction === 0) {
+        dots[1].classList.add("active");
+    } else if (direction < 0) {
+        dots[0].classList.add("active");
+    } else {
+        dots[2].classList.add("active");
+    }
+
 }
